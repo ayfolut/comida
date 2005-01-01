2c39d6988a1c367c19f0cd5d96612604dcef0892
tobesh01 <animashauntaofiq@yahoo.com>
2005-01-01 02:49:35 +0100

    Just Changes

--- static/js/main.js
+++ static/js/main.js
@@ -1,43 +1,24 @@
-// Responsive Nav
-$(function() {
-    menu = $('nav ul');
+// Sidenav
+const sideNav = document.querySelector('.sidenav');
+M.Sidenav.init(sideNav, {});
 
-    $('#openup').on('click', function(e) {
-        e.preventDefault();
-        menu.slideToggle();
+// Slider
+$(document).ready(function() {
+    $('.slider').slider({
+        indicators: false,
+        interval: 3000,
     });
-
-    $(window).resize(function() {
-        var w = $(this).width();
-        if (w > 480 && menu.is(':hidden')) {
-            menu.removeAttr('style');
-        }
-    });
-
-    $('nav li').on('click', function(e) {
-        var w = $(window).width();
-        if (w < 480) {
-            menu.slideToggle();
-        }
-    });
-
-    $('.open-menu').height($(window).height());
 });
 
-// Smooth Scrolling
-$('.cf a').on('click', function(event) {
-    if (this.hash !== '') {
-        event.preventDefault();
+// Material Boxed
+const mb = document.querySelectorAll('.materialboxed');
+M.Materialbox.init(mb, {});
 
-        const hash = this.hash;
+// ScrollSpy
+const ss = document.querySelectorAll('.scrollspy');
+M.ScrollSpy.init(ss, {});
 
-        $('html, body').animate({
-                scrollTop: $(hash).offset().top,
-            },
-            800,
-            function() {
-                window.location.hash = hash;
-            }
-        );
-    }
+// Select
+$(document).ready(function() {
+    $('select').formSelect();
 });

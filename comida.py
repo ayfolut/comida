2f9f20a63a87895274754925c9a89ef814d19d19
Sarah Unokerieghan <sarahunoke@gmail.com>
2020-11-15 02:52:17 +0100

    gitignore work nau finally

---
+++ comida.py
@@ -0,0 +1,9 @@
+import os
+from comida import create_app, db
+
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(app=app, db=db)

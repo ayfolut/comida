b211ad9f25528439c5642caebaf8adbc7a3534e0
Sarah Unokerieghan <sarahunoke@gmail.com>
2020-11-15 04:30:47 +0100

    Trying something

--- comida/auth/routes.py
+++ comida/auth/routes.py
@@ -3,7 +3,7 @@ from flask import redirect, render_template, url_for, current_app, session, requ
 
 
 
-
+@bp.route('/')
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')

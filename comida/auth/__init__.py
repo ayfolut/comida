4bb2c9968d98dda8d693791f16bade5c1b781ce3
Sarah Unokerieghan <sarahunoke@gmail.com>
2020-11-15 02:50:35 +0100

    gitignore work nau

--- comida/auth/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, Flask
-
-bp = Blueprint('auth', __name__, template_folder='templates', static_folder='static')
-
-
-
-from comida.auth import routes
-
-
--- comida/auth/routes.py
+++
@@ -1,9 +0,0 @@
-from . import bp
-from flask import redirect, render_template, url_for, current_app, session, request
-
-
-
-
-@bp.route('/login', methods=['GET', 'POST'])
-def login():
-    return render_template('login.html')

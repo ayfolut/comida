24323b32abbc07e8e46d91a73974ddd0da044a8c
Sarah Unokerieghan <sarahunoke@gmail.com>
2020-11-18 03:15:27 +0100

    Integrating changes

--- comida/email/email.py
+++ comida/email/email.py
@@ -19,7 +19,7 @@ def send_email(subject, sender, recipients, text_body, html_body):
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
     send_email('[COMIDA] Reset Your Password',
-    sender=current_app.config['ADMINS'],
+    sender=current_app.config['COMIDA_ADMIN'][0],
     recipients=[user.email],
     text_body=render_template('email/reset_password.txt', user=user, token=token),
     html_body=render_template('email/reset_password.html', user=user, token=token))

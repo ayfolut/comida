a39037188a71ab0a9b7adfd7b54256d415d4f415
Sarah Unokerieghan <sarahunoke@gmail.com>
2020-11-19 10:40:15 +0100

    Integrating changes

--- comida/main/routes.py
+++ comida/main/routes.py
@@ -8,17 +8,14 @@ from flask_login import login_required
 
 @root.route('/')
 @root.route('/home')
-@login_required
 
 def index():
     return render_template ('index.html')
 
 @root.route('/profile')
-@login_required
 def profile():
     return render_template('profile.html')
 
 @root.route('/pay')
-@login_required
 def pay():
     return render_template('pay.html')
